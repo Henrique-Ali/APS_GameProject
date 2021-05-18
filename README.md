aa5bc119fd13e915c4c2436e15c7571495176729
Henrique-Ali <henriqueali14@outlook.com>
2021-05-17 21:30:55 -0300

    Atualização readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# APS_GameProject
+# UNIP_APS_SEMESTRE_3
+Trabalho do 3° semestre da falculdade Unip em 2021
+
+A proposta do trabalho é a criação de um jogo na liguagem JAVA utilizando interface grafica como SWING, AWT entre outras, o jogo deve conter o tema de reserva ambiental em vista à uma grande metropoles
+
+Desenvolvedores: Caio Pereira, João Stabile, Rebecca Amaral, e Tauan Souza
+
+A ideia do nosso trabalho se consiste em jogo 2D de Tower Defense, muito inspirado no Plants vs. Zombies, com o tema de parques ambientais sendo consumidos pelas grandes metropoles.
+
+Você como jogador deve possicionar os animas e plantas para proteger a floresta da entrada dos humanos.
--- src/Pck_Game/GamePanel.java
+++ src/Pck_Game/GamePanel.java
@@ -15,8 +15,8 @@ import javax.swing.Timer;
 
 public class GamePanel extends JPanel implements ActionListener {
 	private ArrayList<Inimigo> inimigos = new ArrayList<Inimigo>();
-	private static final int SCREEN_WIDTH = 1200;
-	private static final int SCREEN_HEIGHT = 800;
+	private static final int SCREEN_WIDTH = 1280;
+	private static final int SCREEN_HEIGHT = 720;
 	private Timer timer, timerSpawn;
 	private Random random;
 
@@ -24,6 +24,7 @@ public class GamePanel extends JPanel implements ActionListener {
 		this.setBackground(Color.black);
 		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setFocusable(true);
+		
 		timer = new Timer(1, this);
 		timer.start();
 		timerSpawn = new Timer(1000, this);
@@ -36,43 +37,46 @@ public class GamePanel extends JPanel implements ActionListener {
 	public void paint(Graphics g) {
 		super.paint(g);
 //////////////////////////////////////////////////////////////////////////////////
+		
 		Graphics2D g2d = (Graphics2D) g;
 		
 		g.setColor(new Color(100, 200, 255));
-		g.fillRect(0, 0, SCREEN_WIDTH, 300);
+		g.fillRect(0, 0, SCREEN_WIDTH, 220);
 		
 		g.setColor(new Color(50, 100, 50));
-		g.fillRect(0, 300, 500, SCREEN_HEIGHT-300);
-		
-		g.setColor(new Color(40, 90, 40));
-		g2d.setStroke(new BasicStroke(4));
-		g2d.drawLine(0+2, 300+2, 500-2, 300+2);
-		g2d.drawLine(0+2, 400+2, 500-2, 400+2);
-		g2d.drawLine(0+2, 500+2, 500-2, 500+2);
-		g2d.drawLine(0+2, 600+2, 500-2, 600+2);
-		g2d.drawLine(0+2, 700+2, 500-2, 700+2);
-		
-		g2d.drawLine(0+2, 300+2, 0+2, SCREEN_HEIGHT-2);
-		g2d.drawLine(100+2, 300+2, 100+2, SCREEN_HEIGHT-2);
-		g2d.drawLine(200+2, 300+2, 200+2, SCREEN_HEIGHT-2);
-		g2d.drawLine(300+2, 300+2, 300+2, SCREEN_HEIGHT-2);
-		g2d.drawLine(400+2, 300+2, 400+2, SCREEN_HEIGHT-2);
+		g.fillRect(0, 220, SCREEN_WIDTH-378, SCREEN_HEIGHT-220);
 		
 		g.setColor(new Color(40, 40, 50));
-		g.fillRect(500, 300, SCREEN_WIDTH-500, SCREEN_HEIGHT-300);
-		g.setColor(new Color(30, 30, 40));
-		g2d.drawLine(500+2, 300+2, SCREEN_WIDTH-2, 300+2);
-		g2d.drawLine(500+2, 400+2, SCREEN_WIDTH-2, 400+2);
-		g2d.drawLine(500+2, 500+2, SCREEN_WIDTH-2, 500+2);
-		g2d.drawLine(500+2, 600+2, SCREEN_WIDTH-2, 600+2);
-		g2d.drawLine(500+2, 700+2, SCREEN_WIDTH-2, 700+2);
-//////////////////////////////////////////////////////////////////////////////////
+		g.fillRect(SCREEN_WIDTH-380, 220, SCREEN_WIDTH - 900, SCREEN_HEIGHT-220);
 		
+		g2d.setStroke(new BasicStroke(4));
+		g2d.setColor(new Color(30, 30, 40));
+		
+		for(int i = 0; i < 5; i++) {
+			g2d.drawLine(SCREEN_WIDTH-380, 242+(100*i), SCREEN_WIDTH, 242+(100*i));
+		}
+		
+		g2d.setColor(new Color(40, 90, 40));
+		//Linha Vertical
+		for(int i = 0; i < 8; i++) {
+			g2d.drawLine(200-2+(i*100), 240+2, 200-2+(i*100), SCREEN_HEIGHT-80);
+		}
+		
+		//Linha Horizontal
+		for(int i = 0; i < 5; i++) {
+			g2d.drawLine(200, 242+(100*i), 900-2, 242+(100*i));
+		}
+		
+//////////////////////////////////////////////////////////////////////////////////
 		
 		for(int i = 0; i < inimigos.size(); i++) {
 			Inimigo ind = inimigos.get(i);
 			ind.draw(g);
 		}
+		
+		g.setColor(new Color(176, 109, 76));
+		g.fillRect(0, 80, 150, SCREEN_HEIGHT - 160);
+		
 	}
 
 	@Override
@@ -84,7 +88,7 @@ public class GamePanel extends JPanel implements ActionListener {
 				inimigos.remove(ind);			}
 		}
 		if(e.getSource().equals(timerSpawn)) {
-			inimigos.add(new Inimigo(SCREEN_WIDTH-100, 300 + random.nextInt(5)*100));
+			inimigos.add(new Inimigo(SCREEN_WIDTH-50, 240 + random.nextInt(4)*100));
 		}
 	}
 
